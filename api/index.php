944f4555c85fcecfb8e4a902f6ee73851f8a8e5c
Riccardo15Pinto <riccardopinto59@gmail.com>
2023-07-20 14:51:50 +0200

    alert type of answer

--- api/index.php
+++ api/index.php
@@ -6,3 +6,5 @@ $database_path = __DIR__ . '../data/tasks.json';
 $json_data = file_get_contents($database_path);
 
 $tasks = json_decode($json_data, true);
+
+header('Content-Type: application/json');
