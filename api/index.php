08ce3c244b65ed0814585853978225d91fc43023
Riccardo15Pinto <riccardopinto59@gmail.com>
2023-07-20 17:31:22 +0200

    add function

--- api/index.php
+++ api/index.php
@@ -1,12 +1,34 @@
 <?php
 
-
 $database_path = __DIR__ . '/../data/tasks.json';
 
 $json_data = file_get_contents($database_path);
 
 $tasks = json_decode($json_data, true);
 
-header('Content-Type: application/json');
+function get_new_id($tasks)
+{
+    $highest_value = 0;
+    foreach ($tasks as $task) {
+        if ($task["id"] > $highest_value) $highest_value = $task["id"];
+    }
+    return $highest_value + 1;
+}
 
-echo json_encode($tasks);
+
+$new_element = $_POST['skill'] ?? null;
+
+if ($new_element) {
+
+    $tasks[] = ["name" => $new_element, "id" => get_new_id($tasks), "isUse" => false];
+
+    $json_tasks = json_encode($tasks);
+    file_put_contents($database_path, $json_tasks);
+    header('Content-Type: application/json');
+    echo $tasks;
+} else {
+
+    header('Content-Type: application/json');
+
+    echo json_encode($tasks);
+}
